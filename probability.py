f3230b1c3a034da6d88b01045516d6bc923e3f84
P.M.Ngugi <mngugi7@gmail.com>
2025-08-11 20:38:48 +0300

    .ipynb_checkpoints/probability-checkpoint.py

---
+++ probability.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d1246502-65f7-4d5f-b98c-db1d2a8e8dfa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def conditional_probability(p_a_and_b, p_a):\n",
+    "    if p_a == 0:\n",
+    "        return 0\n",
+    "    return p_a_and_b / p_a\n",
+    "\n",
+    "def joint_probability(p_a, p_b):\n",
+    "    return p_a * p_b\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
